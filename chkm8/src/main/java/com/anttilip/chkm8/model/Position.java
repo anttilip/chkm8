357abb79ddb281a0aa5a67bc187183adcccbe8a2
antti.lip@gmail.com <antti.lip@gmail.com>
2016-11-19 05:05:52 +0200

    Add more flexible methods to Position

--- chkm8/src/main/java/com/anttilip/chkm8/model/Position.java
+++ chkm8/src/main/java/com/anttilip/chkm8/model/Position.java
@@ -1,8 +1,10 @@
 package com.anttilip.chkm8.model;
 
 public class Position {
-    private final int x;
-    private final int y;
+    private static final String BOARD_LETTERS = "ABCDEFGH";
+    private int x;
+    private int y;
+
     
     public Position(int x, int y) {
         this.x = x;
@@ -18,31 +20,39 @@ public class Position {
     }
     
     public boolean onBoard() {
-        return (this.x >= 0 && this.x < 8 && this.y >= 0 && this.y < 8);
+        return (this.x >= 0 && this.x < Board.BOARD_SIZE && this.y >= 0 && this.y < Board.BOARD_SIZE);
     }
     
+    public Position add(int x, int y) {
+        this.x += x;
+        this.y += y;
+        return this;
+    }
+
+    public Position copy() {
+        return new Position(this.x, this.y);
+    }
+
+    public static Position add(Position a, Position b) {
+        return new Position(a.getX() + b.getX(), a.getY() + b.getY());
+    }
+
     public String toString() {
-        String alphabets = "ABCDEFGH";
-        return alphabets.charAt(this.x) + Integer.toString(y + 1);
+        return BOARD_LETTERS.charAt(this.x) + Integer.toString(y + 1);
     }
-    
+
     @Override
     public boolean equals(Object o) {
         if (!(o instanceof Position)) {
             return false;
         }
         Position other = (Position) o;
-        
         return other.x == this.x && other.y == this.y;
     }
-    
+
     @Override
     public int hashCode() {
         return 11 * this.x + this.y;
-    }
-    
-    public static Position add(Position a, Position b) {
-        return new Position(a.getX() + b.getX(), a.getY() + b.getY());
     }
 
 }
